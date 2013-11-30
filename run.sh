9eac0048fe35f3267ceff4aaba7904551afb8ebd
Luke Vivier <luke@vivier.ca>
2013-11-29 17:34:34 -0800

    allow global npm installs
    if the `-g` is passed in with options, ownership of /usr/local will be given to the current user so global installs will work.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 mkdir -p $WERCKER_CACHE_DIR/wercker/npm
+
+if [[ "$WERCKER_NPM_INSTALL_OPTIONS" =~ "-g" ]]; then
+  sudo chown -R $USER /usr/local
+fi
+
 npm config set cache $WERCKER_CACHE_DIR/wercker/npm
 npm install $WERCKER_NPM_INSTALL_OPTIONS
